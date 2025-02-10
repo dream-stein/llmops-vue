557c9d8cd16d2dec7a9ffd4ddf1b4c167e7c9073
emcikem <2287697454@qq.com>
2025-02-11 00:09:53 +0800

    feat(.service):账号

--- src/views/pages/HomeView.vue
+++ src/views/pages/HomeView.vue
@@ -1,13 +1,18 @@
 <script setup lang="ts">
-import { useCounterStore } from '@/stores/counter'
+import { useAccountStore } from '@/stores/account'
 
-const counterStore = useCounterStore()
+const accountStore = useAccountStore()
+
+function updateName() {
+  accountStore.update({ name: '小小课' })
+}
 </script>
 <template>
   <p>这是主页</p>
-  <p>数值是:{{ counterStore.count }}</p>
-  <p>双倍数值：{{ counterStore.doubleCount }}</p>
-  <a-button type="primary" @click="counterStore.increment">数值+1</a-button>
+  <p>当前登陆的账号是：{{ accountStore.account.name }}</p>
+  <p>当前登陆的邮箱是: {{ accountStore.account.email }}</p>
+  <a-button type="primary" @click="updateName">更新名字</a-button>
+  <a-button @click="accountStore.clear">重置</a-button>
   <router-link to="/space/apps">跳转到应用列表</router-link>
 </template>
 
