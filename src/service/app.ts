d64837e07260a0cc6cbed7e88b16e4cd8ac76c80
emcikem <2287697454@qq.com>
2025-03-01 16:42:33 +0800

    feat(.service): 触发流水线

--- src/service/app.ts
+++ src/service/app.ts
@@ -1,14 +1,8 @@
 import { post } from '@/util/request.ts'
 import { type DebugAppResponse } from '@/models/app.ts'
 
-export const debugApp = (tenantId: number, prompt: string) => {
-  return post<DebugAppResponse>(`/assistant/chat`, {
-    body: {
-      tenantId: tenantId,
-      dialogId: 1,
-      modelType: 2,
-      prompt: prompt,
-      isOnline: false,
-    },
+export const debugApp = (app_id: string, query: string) => {
+  return post<DebugAppResponse>(`/apps/${app_id}/debug`, {
+    body: { query },
   })
 }
