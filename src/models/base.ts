3569feb879bf9d485d6322b14f206bd8579772c4
emcikem <2287697454@qq.com>
2025-03-16 21:31:21 +0800

    feat(.service): 应用列表模型

--- src/models/base.ts
+++ src/models/base.ts
@@ -5,7 +5,13 @@ export type BaseResponse<T> = {
   data: T
 }
 
-// 基础凡爷相应数据格式
+// 基础分页数据结构
+export type BasePaginatorRequest = {
+  current_page: number
+  page_size: number
+}
+
+// 基础分页响应数据格式
 export type BasePaginatorResponse<T> = BaseResponse<{
   list: Array<T>
   paginator: {
