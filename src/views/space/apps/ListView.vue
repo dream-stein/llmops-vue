8533e57a461413dc865ac0b6e729ac8e449b767f
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-03-19 11:13:37 +0800

    Update ListView.vue

--- src/views/space/apps/ListView.vue
+++ src/views/space/apps/ListView.vue
@@ -3,6 +3,10 @@
 <template>
   <p>这里是AI应用列表</p>
   <router-link to="/home">返回主页</router-link>
+  <router-link :to="{
+                    name: 'space-apps-detail',
+                    params: { app_id: '211221' },
+                  }">去应用页</router-link>
 </template>
 
 <style scoped></style>
