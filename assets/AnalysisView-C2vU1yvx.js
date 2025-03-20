ce0b823f7c32a4580f39a3293a2aac72c199b0a2
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-03-20 14:08:59 +0000

    Deploying to static-pages from @ dream-stein/llmops-vue@a2e713966aff8594d5ff46071d318f7f355f4afa 🚀

--- assets/AnalysisView-C2vU1yvx.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,f as s,o as c}from"./index-4LsbD6di.js";const o={},t={class:""};function n(r,_){return c(),s("div",t,"统计分析页面")}const f=e(o,[["render",n]]);export{f as default};
---
+++ assets/oauth-BhXXRoGU.js
@@ -0,0 +1,1 @@
+import{p as r,l as a}from"./index-D2iA5eqK.js";const s=o=>a(`/oauth/${o}`),p=(o,t)=>r(`/oauth/authorize/${o}`,{body:{code:t}});export{p as a,s as p};
