f30c77eaaa8bdf3175dece2e3c2c5aa2e9ba1473
emcikem <2287697454@qq.com>
2025-03-21 22:21:45 +0800

    feat(.service): 应用插件

--- src/hooks/use-tools.ts
+++ src/hooks/use-tools.ts
@@ -1,6 +1,6 @@
 import { reactive, ref } from 'vue'
-import { getApiTool } from '@/service/api-tools.ts'
-import { getBuiltinTool, getBuiltinTools } from '@/service/builtin-tool.ts'
+import { getApiTool, getApiToolProvidersWithPage } from '@/service/api-tools.ts'
+import { type GetApiToolProvidersWithPageResponse } from '@/models/api-tool.ts'
 
 export const useGetApiTool = () => {
   // 1. 定义hooks所需数据
@@ -23,56 +23,64 @@ export const useGetApiTool = () => {
   return { loading, api_tool, loadApiTool }
 }
 
-export const useGetBuiltinTool = () => {
-  // 1. 定义hooks所需数据
+export const useGetApiToolProvidersWithPage = () => {
+  // 1.定义hooks所需数据
   const loading = ref(false)
-  const builtin_tool = reactive<Record<string, any>>({})
+  const api_tool_providers = reactive<GetApiToolProvidersWithPageResponse['data']['list']>([])
+  const defaultPaginator = {
+    current_page: 1,
+    page_size: 20,
+    total_page: 0,
+    total_record: 0,
+  }
+  const paginator = reactive({ ...defaultPaginator })
 
-  // 2. 定义加载函数
-  const loadBuiltinTool = async (provider_name: string, tool_name: string) => {
+  // 2.定义加载数据函数
+  const loadApiToolProviders = async (init: boolean = false) => {
+    // 2.1 判断是否是初始化，如果是的话则先初始化分页器
+    if (init) {
+      Object.assign(paginator, { ...defaultPaginator })
+    } else if (paginator.current_page > paginator.total_page) {
+      return
+    }
+
+    // 2.2 加载更多数据并更新数据状态
     try {
+      // 2.3 调用接口获取响应数据
       loading.value = true
-      const resp = await getBuiltinTool(provider_name, tool_name)
+      const resp = await getApiToolProvidersWithPage(
+        paginator.current_page,
+        paginator.page_size,
+        '',
+      )
       const data = resp.data
 
-      Object.assign(builtin_tool, data)
+      // 2.4 更新分页器
+      updatePaginator(data)
+
+      // 2.5 判断是否存在更多数据
+      if (paginator.current_page <= paginator.total_page) {
+        paginator.current_page += 1
+      }
+
+      // 2.6 追加或者是覆盖数据
+      if (init) {
+        api_tool_providers.splice(0, api_tool_providers.length, ...data.list)
+      } else {
+        api_tool_providers.push(...data.list)
+      }
     } finally {
-      Object.assign(builtin_tool, {
-        name: 'tool',
-        label: 'tool',
-        description: '一个用于执行Google SERP搜索并提取片段和网页的工具。输入应该是一个搜索查询。',
-        icon: 'https://q2.itc.cn/q_70/images03/20240329/84cca9641e9444e5b3fe9256f095da6b.png',
-        provider: {
-          name: 'tool_name',
-          label: 'tool_name',
-          category: 'tool',
-          background: 'saa',
-          description:
-            '一个用于执行Google SERP搜索并提取片段和网页的工具。输入应该是一个搜索查询。',
-        },
-        params: [
-          {
-            name: 'tool_name',
-            label: 'tool_name',
-            type: 'string',
-            required: true,
-            min: 1,
-            max: 10,
-            options: [],
-          },
-        ],
-        inputs: [
-          {
-            type: 'str',
-            name: 'name',
-            required: true,
-            default: 'tool_name',
-          },
-        ],
-      })
       loading.value = false
     }
   }
 
-  return { loading, builtin_tool, loadBuiltinTool }
+  // 3.定义更新分页器函数
+  const updatePaginator = (data: any) => {
+    paginator.current_page = data.paginator.current_page
+    paginator.page_size = data.paginator.page_size
+    paginator.total_page = data.paginator.total_page
+    paginator.total_record = data.paginator.total_record
+  }
+
+  return { loading, api_tool_providers, paginator, loadApiToolProviders }
 }
