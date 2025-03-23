6ad8ea2aaee9a25a9ee39df5481a576ec708909c
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-03-23 16:28:06 +0000

    Deploying to static-pages from @ dream-stein/llmops-vue@11918151d19608e114d06e5604d5ce18b33441b8 🚀

---
+++ assets/oauth-BHgTqZKa.js
@@ -0,0 +1,1 @@
+import{D as r,E as a}from"./index-DCl4em1w.js";const s=o=>a(`/oauth/${o}`),u=(o,t)=>r(`/oauth/authorize/${o}`,{body:{code:t}});export{u as a,s as p};
--- assets/oauth-DekULZSd.js
+++
@@ -1,1 +0,0 @@
-import{D as r,E as a}from"./index-Dskj8Wgr.js";const s=o=>a(`/oauth/${o}`),u=(o,t)=>r(`/oauth/authorize/${o}`,{body:{code:t}});export{u as a,s as p};
