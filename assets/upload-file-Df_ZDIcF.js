0b5e2a5d76fc52534d7f1608ddae6d36ea6a1a22
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-03-24 17:33:03 +0000

    Deploying to static-pages from @ dream-stein/llmops-vue@d658f2f8f5c4525d9d0a0663a6e9306150c5c96d 🚀

--- assets/upload-file-Df_ZDIcF.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-B3I-rsif.js";const l=o=>{const a=new FormData;return a.append("file",o),e("/upload-files/image",{data:a})},p=o=>{const a=new FormData;return a.append("file",o),e("/upload-files/file",{data:a})};export{p as a,l as u};
