908df650061c47538ae7f210f24957535d3d29eb
emcikem <2287697454@qq.com>
2025-03-25 02:34:28 +0800

    feat(.service): 应用列表 新建弹窗

--- src/views/space/apps/component/CreateOrUpdateAppModal.vue
+++ src/views/space/apps/component/CreateOrUpdateAppModal.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 import { ref, watch } from 'vue'
 import { type Form, type ValidatedError } from '@arco-design/web-vue'
-import { useCreateApp, useGetApp, useUpdateApp } from '@/hooks/use-app'
+import { useCreateApp, useGetApp, useUpdateApp } from '@/hooks/use-app.ts'
+import { uploadImage } from '@/service/upload-file.ts'
 
 // 1.定义自定义组件所需数据
 const props = defineProps({
@@ -10,9 +11,68 @@ const props = defineProps({
   callback: { type: Function, required: false },
 })
 const emits = defineEmits(['update:visible', 'update:app_id'])
+const { loading: createAppLoading, handleCreateApp } = useCreateApp()
+const { loading: updateAppLoading, handleUpdateApp } = useUpdateApp()
+const { app, loadApp } = useGetApp()
+const defaultForm = {
+  fileList: [] as any,
+  icon: '',
+  name: '',
+  description: '',
+}
+const form = ref({ ...defaultForm })
+const formRef = ref<InstanceType<typeof Form>>()
 
 // 2.定义隐藏模态窗函数
 const hideModal = () => emits('update:visible', false)
+
+// 3.定义表单提交函数
+const saveApp = async ({ errors }: { errors: Record<string, ValidatedError> | undefined }) => {
+  // 3.1 判断表单是否出错
+  if (errors) return
+
+  // 3.2 检测是保存还是新增，调用不同的API接口
+  if (props.app_id) {
+    await handleUpdateApp(props.app_id, form.value)
+  } else {
+    await handleCreateApp(form.value)
+  }
+
+  // 3.3 完成保存操作，隐藏模态窗并调用回调函数
+  emits('update:visible', false)
+  props.callback && props.callback()
+}
+
+// 4.监听模态窗显示状态变化
+watch(
+  () => props.visible,
+  async (newValue) => {
+    // 4.1 清除表单校验信息
+    formRef.value?.resetFields()
+
+    // 4.2 判断弹窗是打开还是关闭
+    if (newValue) {
+      // 4.3 开启弹窗，需要检测下是更新还是创建操作
+      if (props.app_id) {
+        // 4.4 调用接口获取文档片段详情
+        await loadApp(props.app_id)
+
+        // 4.5 更新表单数据
+        form.value = {
+          fileList: [{ uid: '1', name: '应用图标', url: app.value.icon }],
+          icon: app.value.icon,
+          name: app.value.name,
+          description: app.value.description,
+        }
+      }
+    } else {
+      // 4.6 关闭弹窗，需要清空表单数据
+      form.value = defaultForm
+      formRef.value?.resetFields()
+      emits('update:app_id', '')
+    }
+  },
+)
 </script>
 
 <template>
@@ -34,6 +94,50 @@ const hideModal = () => emits('update:visible', false)
           <icon-close />
         </template>
       </a-button>
+    </div>
+    <!-- 中间表单 -->
+    <div class="pt-6">
+      <a-form ref="formRef" :model="form" layout="vertical" @submit="saveApp">
+        <a-form-item
+          field="fileList"
+          hide-label
+          :rules="[{ required: true, message: '应用图标不能为空' }]"
+        >
+          <a-upload />
+        </a-form-item>
+        <a-form-item
+          field="name"
+          label="应用名称"
+          asterisk-position="end"
+          :rules="[{ required: true, message: '应用名称不能为空' }]"
+        >
+          <a-input v-model:model-value="form.name" placeholder="请输入应用名称" />
+        </a-form-item>
+        <a-form-item field="content" label="应用描述">
+          <a-textarea
+            v-model:model-value="form.description"
+            :auto-size="{ minRows: 8, maxRows: 8 }"
+            :max-length="800"
+            show-word-limit
+            placeholder="请输入关于该应用的描述信息"
+          />
+        </a-form-item>
+        <!-- 底部按钮 -->
+        <div class="flex items-center justify-between">
+          <div class=""></div>
+          <a-space :size="16">
+            <a-button class="rounded-lg" @click="hideModal">取消</a-button>
+            <a-button
+              :loading="createAppLoading || updateAppLoading"
+              type="primary"
+              html-type="submit"
+              class="rounded-lg"
+            >
+              保存
+            </a-button>
+          </a-space>
+        </div>
+      </a-form>
     </div>
   </a-modal>
 </template>
