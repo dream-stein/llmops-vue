23e3ce9eae20a0fa42a5a402f259c567715ed8ec
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-03-24 17:26:55 +0000

    Deploying to static-pages from @ dream-stein/llmops-vue@937749b38d31c2ba8a9df6379a0a80f21d1c78f3 🚀

---
+++ assets/builtin-tool-DC4T5Wc6.js
@@ -0,0 +1,1 @@
+import{E as t}from"./index-7gce_Baj.js";const e=()=>t("/builtin-tools/categories"),l=()=>t("/builtin-tools"),n=(o,s)=>t(`/builtin-tools/${o}/tools/${s}`);export{l as a,n as b,e as g};
---
+++ assets/oauth-Bb6-hfm8.js
@@ -0,0 +1,1 @@
+import{D as r,E as a}from"./index-7gce_Baj.js";const s=o=>a(`/oauth/${o}`),u=(o,t)=>r(`/oauth/authorize/${o}`,{body:{code:t}});export{u as a,s as p};
