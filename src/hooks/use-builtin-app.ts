62509215c4e26dd69806fa57c98f3964a59e3b81
emcikem <2287697454@qq.com>
2025-03-25 22:09:36 +0800

    feat(.service): 应用广场列表

---
+++ src/hooks/use-builtin-app.ts
@@ -0,0 +1,79 @@
+import { ref } from 'vue'
+import type {
+  GetBuiltinAppCategoriesResponse,
+  GetBuiltinAppsResponse,
+} from '@/models/builtin-app.ts'
+import {
+  addBuiltinAppToSpace,
+  getBuiltinAppCategories,
+  getBuiltinApps,
+} from '@/service/builtin-app.ts'
+import { useRouter } from 'vue-router'
+import { Message } from '@arco-design/web-vue'
+
+export const useGetBuiltinAppCategories = () => {
+  // 1.定义hooks所需数据
+  const loading = ref(false)
+  const categories = ref<GetBuiltinAppCategoriesResponse['data']>([])
+
+  // 2.定义加载数据函数
+  const loadBuiltinAppCategories = async () => {
+    try {
+      loading.value = true
+      const resp = await getBuiltinAppCategories()
+      categories.value = resp.data
+    } finally {
+      categories.value = [
+        { category: '助手', name: '助手' },
+        { category: '人力资源', name: '人力资源' },
+        { category: '写作', name: '写作' },
+        { category: '电商营销', name: '电商营销' },
+      ]
+      loading.value = false
+    }
+  }
+
+  return { loading, categories, loadBuiltinAppCategories }
+}
+
+export const useGetBuiltinApps = () => {
+  // 1.定义hooks所需数据
+  const loading = ref(false)
+  const apps = ref<GetBuiltinAppsResponse['data']>([])
+
+  // 2.定义加载数据函数
+  const loadBuiltinApps = async () => {
+    try {
+      loading.value = true
+      const resp = await getBuiltinApps()
+      apps.value = resp.data
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return { loading, apps, loadBuiltinApps }
+}
+
+export const useAddBuiltinAppToSpace = () => {
+  // 1.定义hooks所需数据
+  const router = useRouter()
+  const loading = ref(false)
+
+  // 2.定义处理器函数
+  const handleAddBuiltinAppToSpace = async (builtin_app_id: string) => {
+    try {
+      loading.value = true
+      const resp = await addBuiltinAppToSpace(builtin_app_id)
+      Message.success('将Agent模板添加到工作区成功')
+      await router.push({
+        name: 'space-apps-detail',
+        params: { app_id: resp.data.id },
+      })
+    } finally {
+      loading.value = false
+    }
+  }
+
+  return { loading, handleAddBuiltinAppToSpace }
+}
---
+++ src/service/builtin-app.ts
@@ -0,0 +1,23 @@
+import { get, post } from '@/util/request.ts'
+import type {
+  GetBuiltinAppCategoriesResponse,
+  GetBuiltinAppsResponse,
+} from '@/models/builtin-app.ts'
+import type { BaseResponse } from '@/models/base.ts'
+
+// 获取内置应用分类列表
+export const getBuiltinAppCategories = () => {
+  return get<GetBuiltinAppCategoriesResponse>(`/builtin-apps/categories`)
+}
+
+// 获取内置应用列表信息
+export const getBuiltinApps = () => {
+  return get<GetBuiltinAppsResponse>(`/builtin-apps`)
+}
+
+// 将指定 Agent 模板添加到工作区
+export const addBuiltinAppToSpace = (builtin_app_id: string) => {
+  return post<BaseResponse<{ id: string }>>(`/builtin-apps/add-builtin-app-to-space`, {
+    body: { builtin_app_id },
+  })
+}
