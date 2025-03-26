020bbb7d2f4f5c55020535bd46185a697d2b50c2
emcikem <2287697454@qq.com>
2025-03-27 00:43:21 +0800

    feat(.service):新建工作流node和info类 && 主页样式

--- src/views/pages/HomeView.vue
+++ src/views/pages/HomeView.vue
@@ -1,7 +1,131 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { nextTick, onMounted, ref } from 'vue'
+import AssistantAgentBackground from '@/assets/images/assistant-agent-background.png'
+
+// 1.定义页面所需数据
+const query = ref('')
+const task_id = ref('')
+const messages = ref([])
+const opening_questions = ['什么是LLMOps?', '我想创建一个应用', '能介绍下什么是RAG吗?']
+</script>
 
 <template>
-  <router-link to="/space/apps">跳转到应用列表</router-link>
+  <div
+    class="w-full h-full min-h-screen bg-gray-100 bg-cover bg-no-repeat bg-center"
+    :style="{ backgroundImage: `url(${AssistantAgentBackground})` }"
+  >
+    <!-- 中间页面信息 -->
+    <div class="w-[600px] h-full min-h-screen mx-auto">
+      <!-- 历史对话记录 -->
+      <div
+        v-if="messages.length > 0"
+        class="flex flex-col px-6 h-[calc(100%-100px)] min-h-[calc(100vh-100px)]"
+      ></div>
+      <!-- 对话记录为空时展示的对话开场白 -->
+      <div
+        v-else
+        class="flex flex-col p-6 gap-2 items-center justify-center overflow-scroll scrollbar-w-none h-[calc(100%-100px)] min-h-[calc(100vh-100px)]"
+      >
+        <div class="mb-9">
+          <div class="text-[40px] font-bold text-gray-700 mt-[52px] mb-4">
+            Hi，我是 AI 应用构建器
+          </div>
+          <div class="text-[30px] font-bold text-gray-700 mb-2">
+            你的专属
+            <span class="text-blue-700">AI 原生应用</span>
+            开发平台
+          </div>
+          <div class="text-base text-gray-700">
+            说出你的创意，我可以快速帮你创建专属应用，一键轻松分享给朋友，也可以一键发布到 LLMOps
+            平台、微信等多个渠道。
+          </div>
+        </div>
+        <!-- 开场AI对话消息 -->
+        <div class="flex gap-2">
+          <!-- 左侧图标 -->
+          <a-avatar :size="30" shape="circle" class="flex-shrink-0 bg-blue-700">
+            <icon-apps />
+          </a-avatar>
+          <!-- 右侧名称与消息 -->
+          <div class="flex flex-col items-start gap-2">
+            <!-- 应用名称 -->
+            <div class="text-gray-700 font-bold">辅助Agent</div>
+            <!-- AI消息 -->
+            <div
+              class="bg-white border border-gray-200 text-gray-700 px-4 py-3 rounded-2xl break-all leading-7"
+            >
+              <div class="font-bold">你好，欢迎来到LLMOps🎉</div>
+              <div class="">
+                LLMOps是新一代大模型 AI 应用开发平台。无论你是否有编程基础，都可以快速搭建出各种 AI
+                应用，并一键发布到各大社交平台，或者轻松部署到自己的网站。
+              </div>
+              <ul class="list-disc pl-6">
+                <li>
+                  随时来
+                  <router-link :to="{ name: 'store-apps-list' }" class="text-blue-700"
+                    >应用广场
+                  </router-link>
+                  逛逛，这里内置了许多超有趣的应用。
+                </li>
+                <li>你也可以直接发送“我想做一个应用”，我可以帮你快速创建应用。</li>
+                <li>你也可以向我提问有关课程的问题，我可以快速替你解答。</li>
+              </ul>
+              <div class="">如果你还有其他LLMOps使用问题，也欢迎随时问我！</div>
+            </div>
+            <!-- 开场白建议问题 -->
+            <div class="flex flex-col gap-2">
+              <div
+                v-for="(opening_question, idx) in opening_questions"
+                :key="idx"
+                class="px-4 py-1.5 border rounded-lg text-gray-700 cursor-pointer bg-white hover:bg-gray-50"
+                @click="() => {}"
+              >
+                {{ opening_question }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 对话输入框 -->
+      <div class="w-full flex flex-col flex-shrink-0">
+        <!-- 顶部输入框 -->
+        <div class="px-6 flex items-center gap-4">
+          <!-- 清除按钮 -->
+          <a-button
+            class="flex-shrink-0 !text-gray-700"
+            type="text"
+            shape="circle"
+            @click="() => {}"
+          >
+            <template #icon>
+              <icon-empty :size="16" />
+            </template>
+          </a-button>
+          <!-- 输入框组件 -->
+          <div
+            class="bg-white h-[50px] flex items-center gap-2 px-4 flex-1 border border-gray-200 rounded-full"
+          >
+            <input
+              v-model="query"
+              type="text"
+              class="flex-1 outline-0"
+              placeholder="发送消息或创建AI应用..."
+              @keyup.enter="() => {}"
+            />
+            <a-button type="text" shape="circle" class="!text-gray-700" @click="() => {}">
+              <template #icon>
+                <icon-send :size="16" />
+              </template>
+            </a-button>
+          </div>
+        </div>
+        <!-- 底部提示消息 -->
+        <div class="text-center text-gray-500 text-xs py-4">
+          内容由AI生成，无法确保真实准确，仅供参考。
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
