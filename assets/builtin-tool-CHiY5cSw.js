af54a1a3aac1c798653a39cc9b47efde9f555359
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-03-29 16:59:19 +0000

    Deploying to static-pages from @ dream-stein/llmops-vue@f15a2953c4b0cbf392690cc6536bf53bbf94cba7 🚀

---
+++ assets/builtin-tool-CHiY5cSw.js
@@ -0,0 +1,1 @@
+import{g as t}from"./index-RXUMZQy2.js";const e=()=>t("/builtin-tools/categories"),l=()=>t("/builtin-tools"),n=(o,s)=>t(`/builtin-tools/${o}/tools/${s}`);export{l as a,n as b,e as g};
