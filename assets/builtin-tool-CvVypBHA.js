13fb4d66ab75dc9d87c290646807efa43e9d0a79
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-03-31 17:11:31 +0000

    Deploying to static-pages from @ dream-stein/llmops-vue@f23c50a699ea7ffd38330d6988497d0adc1e35d5 🚀

--- assets/builtin-tool-CvVypBHA.js
+++
@@ -1,1 +0,0 @@
-import{g as t}from"./index-ipiVb2YE.js";const e=()=>t("/builtin-tools/categories"),l=()=>t("/builtin-tools"),n=(o,s)=>t(`/builtin-tools/${o}/tools/${s}`);export{l as a,n as b,e as g};
---
+++ assets/upload-file-C8roHYFz.js
@@ -0,0 +1,1 @@
+import{I as e}from"./index-BLJetADW.js";const l=o=>{const a=new FormData;return a.append("file",o),e("/upload-files/image",{data:a})},p=o=>{const a=new FormData;return a.append("file",o),e("/upload-files/file",{data:a})};export{p as a,l as u};
