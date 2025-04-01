7a3ece04637ae3478dc980f978a4a30ac903a9ca
Emcikem <48344553+Emcikem@users.noreply.github.com>
2025-04-01 15:29:47 +0000

    Deploying to static-pages from @ dream-stein/llmops-vue@e358c1eb913021108beca1753ee5f240df1e473e 🚀

--- assets/oauth-BB_7KzYM.js
+++
@@ -1,1 +0,0 @@
-import{p as r,g as a}from"./index-CK2Jsrw1.js";const s=o=>a(`/oauth/${o}`),p=(o,t)=>r(`/oauth/authorize/${o}`,{body:{code:t}});export{p as a,s as p};
--- assets/upload-file-B__bpcQm.js
+++
@@ -1,1 +0,0 @@
-import{I as e}from"./index-CK2Jsrw1.js";const l=o=>{const a=new FormData;return a.append("file",o),e("/upload-files/image",{data:a})},p=o=>{const a=new FormData;return a.append("file",o),e("/upload-files/file",{data:a})};export{p as a,l as u};
