4ef2cfda60a31362204e70781d1cf6f80108a084
emcikem <2287697454@qq.com>
2025-09-25 00:30:21 +0800

    fix(.service): app应用修改

--- src/views/space/datasets/documents/ListView.vue
+++ src/views/space/datasets/documents/ListView.vue
@@ -176,7 +176,7 @@ const { handleUpdate: handleUpdateEnabled } = useUpdateDocumentEnabled()
             cell-class="bg-transparent text-gray-700"
           >
             <template #cell="{ record }">
-              {{ moment(record.created_at).format('YYYY-MM-DD HH:mm:ss') }}
+              {{ moment(record.created_at * 1000).format('YYYY-MM-DD HH:mm:ss') }}
             </template>
           </a-table-column>
           <a-table-column
